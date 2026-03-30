6c8b76abba365d2839e6777339806d82e7cd23d8
alexandritem886-dotcom <alexandritem886@gmail.com>
2026-03-30 13:56:55 +0100

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,17 @@
+JavaScript:
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 2 },
+    color: { value: "#ffffff" },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#555",
+      opacity: 0.2
+    },
+    move: {
+      speed: 1
+    }
+  }
+});
